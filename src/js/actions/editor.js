c1c7180a9a4e2e0900d82cf0e8238b2ff778dda8
jshimazu <jshimazu0820@gmail.com>
2017-03-12 19:22:21 +0900

    タイトルを追加

--- src/js/actions/editor.js
+++ src/js/actions/editor.js
@@ -1,5 +1,4 @@
 import { ACTION_TYPE } from '../constants/editor';
-console.log(ACTION_TYPE.CHANGE_HTML);
 
 export const changeDecorateType = (decorateType) => {
     return (
@@ -10,12 +9,36 @@ export const changeDecorateType = (decorateType) => {
     )
 };
 
-export const changeHTML = (html) => {
+export const changeHTML = (beforeHtml) => {
+    const html = filterHtml(beforeHtml);
     return (
         {
             type: ACTION_TYPE.CHANGE_HTML,
-            html
+            html,
         }
     )
 };
 
+
+// TODO dummy
+function filterHtml(beforeHTML) {
+    if (!beforeHTML) {
+        return "";
+    }
+
+    let afterHTML = beforeHTML;
+
+    const twitterUrl = /twitter\.com.+\/status/;
+    const twitterEmbedUrl = '<blockquote class="twitter-tweet" data-lang="ja"><p lang="ja" dir="ltr">ゲームが上手かったら内定出します。一芸ならぬ <a href="https://twitter.com/hashtag/%E3%81%84%E3%81%A1%E3%82%B2%E3%83%BC%E6%8E%A1%E7%94%A8?src=hash">#いちゲー採用</a> を開始しました。「ゲームばかりに時間を費やしてしまった…」というあなた、大歓迎です。ご応募お待ちしています！ <a href="https://twitter.com/hashtag/%E5%B0%B1%E6%B4%BB%E8%A7%A3%E7%A6%81?src=hash">#就活解禁</a> <a href="https://twitter.com/hashtag/%E6%96%B0%E5%8D%92%E6%8E%A1%E7%94%A8%E3%81%AF%E3%81%98%E3%82%81%E3%81%BE%E3%81%97%E3%81%9F?src=hash">#新卒採用はじめました</a> <a href="https://t.co/FNnB5CcAYF">https://t.co/FNnB5CcAYF</a></p>&mdash; 面白法人カヤック (@kayac_inc) <a href="https://twitter.com/kayac_inc/status/836605215461654529">2017年2月28日</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+    if (beforeHTML.match(twitterUrl)) {
+        afterHTML = afterHTML.replace(twitterUrl, twitterEmbedUrl);
+    }
+
+    const youtubeUrl = "youtube.com/watch";
+    const youtubeEmbedUrl = '<iframe width="560" height="315" src="https://www.youtube.com/embed/H5RcVDcGPYE" frameborder="0" allowfullscreen></iframe>';
+    if (beforeHTML.match(youtubeUrl)) {
+        afterHTML = afterHTML.replace(youtubeUrl, youtubeEmbedUrl);
+    }
+
+    return afterHTML;
+};
--- src/js/components/EditorInput/index.js
+++ src/js/components/EditorInput/index.js
@@ -4,6 +4,9 @@
 import { Component, PropTypes } from 'react';
 import style from './style.css';
 import ContentEditable from 'react-contenteditable';
+import _ from 'lodash';
+
+const THROTTLE_TIME = 1000;
 
 export default class extends Component {
     static propTypes = {
@@ -11,33 +14,53 @@ export default class extends Component {
     };
 
     handleChangeDOM = (evt) => {
-        const target = evt.target;
-        const sel = window.getSelection();
-        const x = sel.anchorOffset;
-        const y = sel.focusOffset;
-        console.log(target.selectionStart);
-        console.log(sel);
-        console.log(x, y);
-        const html = evt.target.value;
-        this.props.changeHTML(html);
+        // 間引き
+        _.throttle(() => {
+            const target = evt.target;
+            const sel = window.getSelection();
+            const x = sel.anchorOffset;
+            const y = sel.focusOffset;
+            //console.log(target.selectionStart);
+            //console.log(sel);
+            //console.log(x, y);
+            const html = evt.target.value;
+            this.props.changeHTML(html);
+        }, THROTTLE_TIME)();
     }
 
     handleKeyup = (evt) => {
-        console.log("key up");
+        //console.log("key up");
     }
 
     render() {
         return (
-            <ContentEditable
-                className={style.wrapper}
-                html={this.props.currentHTML}
-                disabled={false}
-                onChange={this.handleChangeDOM}
-                onKeyUp={this.handleKeyup}
-                onBlur={this.handleKeyup}
-                onFocus={this.handleKeyup}
-            >
-            </ContentEditable>
+            <div className={style.wrapper}>
+                {/*}
+                <input type="text" className={style.title} placeholder="title" />
+                {*/}
+                <div className={style.titleWrapper}>
+                    <ContentEditable
+                        className={style.title}                    ref="title"
+                    >
+                    </ ContentEditable>
+                </div>
+                {/*}
+                <div className={style.titleWrapper}>
+                    <textarea className={style.title}></textarea>
+                </div>
+                {*/}
+                <ContentEditable
+                    ref="content"
+                    className={style.content}
+                    html={this.props.currentHTML}
+                    disabled={false}
+                    onChange={this.handleChangeDOM}
+                    onKeyUp={this.handleKeyup}
+                    onBlur={this.handleKeyup}
+                    onFocus={this.handleKeyup}
+                >
+                </ContentEditable>
+            </div>
         );
     }
 }
